286ed33f5b60c85626991800b7573a2a208daa4f
Matus Pohancenik <matuspohancenik@gmail.com>
2021-03-08 22:19:58 +0100

    feat: add implementation of experiment function

--- Cvicenie 03/experiment.py
+++ Cvicenie 03/experiment.py
@@ -7,29 +7,42 @@ NUMBER_OF_PRODUCERS = 5
 NUMBER_OF_CONSUMERS = 5
 PRODUCTION_TIME = randint(0, 10) / 100
 PROCESSING_TIME = randint(0, 10) / 10
+OPERATING_TIME = 0.1
+TIME_DIVIDER = 200
+
+
+class Results:
+    def __init__(self):
+        self.data = []
 
 
 class Warehouse:
-    def __init__(self, capacity):
-        self.end = False
+    def __init__(self):
+        self.closed = False
         self.mutex = Mutex()
         self.items = Semaphore(0)
-        self.free = Semaphore(capacity)
+        self.free = Semaphore(WAREHOUSE_CAPACITY)
+        self.items_produced = 0
+
+    def reset(self):
+        self.closed = False
+        self.items = Semaphore(0)
+        self.free = Semaphore(WAREHOUSE_CAPACITY)
         self.items_produced = 0
 
     def finish(self):
-        self.end = True
-        self.items.signal(50)
-        self.free.signal(50)
+        self.closed = True
+        self.items.signal(100)
+        self.free.signal(100)
 
     def add_product(self):
         self.items_produced += 1
 
 
-def produce(warehouse: Warehouse, thread_index):
+def produce(warehouse: Warehouse, time_to_produce):
     while True:
         # produkcia
-        sleep(PRODUCTION_TIME)
+        sleep(time_to_produce)
         # kontrola volneho miesta v sklade
         warehouse.free.wait()
         # ziskanie vylucneho pristupu do skladu
@@ -42,11 +55,11 @@ def produce(warehouse: Warehouse, thread_index):
         # zvysenie poctu vyrobkov v sklade
         warehouse.items.signal()
 
-        if warehouse.end:
+        if warehouse.closed:
             break
 
 
-def consume(warehouse: Warehouse, thread_index):
+def consume(warehouse: Warehouse):
     while True:
         # kontrola existencie polozky v sklade
         warehouse.items.wait()
@@ -59,31 +72,57 @@ def consume(warehouse: Warehouse, thread_index):
         # uvolnenie miesta v sklade
         warehouse.free.signal()
         # spracovanie polozky
-        sleep(PROCESSING_TIME)
+        sleep(randint(0, 10) / TIME_DIVIDER)
 
-        if warehouse.end:
+        if warehouse.closed:
             break
 
 
-def main():
-    warehouse = Warehouse(WAREHOUSE_CAPACITY)
-    producers = list()
-    consumers = list()
-
-    for i in range(NUMBER_OF_PRODUCERS):
-        producers.append(Thread(produce, warehouse, i))
-
-    for j in range(NUMBER_OF_CONSUMERS):
-        consumers.append(Thread(consume, warehouse, j))
-
-    sleep(2)
-    warehouse.finish()
-
+def wait_to_finish(producers: list, consumers: list):
     for p in producers:
         p.join()
 
     for c in consumers:
         c.join()
+
+
+def main():
+    results = Results()
+    warehouse = Warehouse()
+    producers = list()
+    consumers = list()
+
+    iteration = 0
+    for production_time in range(2, 12):
+        for number_of_producers in range(1, 11):
+            items_produced = 0
+            repetitions = 10
+
+            for rep in range(repetitions):
+                warehouse.reset()
+                consumers.clear()
+                producers.clear()
+
+                for i in range(number_of_producers):
+                    producers.append(
+                        Thread(produce, warehouse,
+                               production_time / TIME_DIVIDER))
+
+                for j in range(NUMBER_OF_CONSUMERS):
+                    consumers.append(
+                        Thread(consume, warehouse))
+
+                sleep(OPERATING_TIME)
+                warehouse.finish()
+                wait_to_finish(producers, consumers)
+
+                items_produced += warehouse.items_produced / OPERATING_TIME
+
+            items_produced_avg = items_produced / repetitions
+            results.data.append((production_time / TIME_DIVIDER,
+                                 number_of_producers, items_produced_avg))
+            iteration += 1
+            print("Iteration {0}/100".format(iteration))
 
 
 if __name__ == "__main__":
