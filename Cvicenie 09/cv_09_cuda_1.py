b77a67e9828f39da5292702a5036c87fd84fc7e2
Matus Pohancenik <matuspohancenik@gmail.com>
2021-04-26 09:08:30 +0200

    feat: add cuda 1d version 1 implementation

---
+++ Cvicenie 09/cv_09_cuda_1.py
@@ -0,0 +1,28 @@
+from __future__ import division
+
+from numba import cuda
+import numpy
+import math
+
+
+@cuda.jit
+def my_kernel(io_array):
+    tx = cuda.threadIdx.x  # pozicia vlakna v bloku
+    ty = cuda.blockIdx.x  # pozicia bloku v gride
+    bw = cuda.blockDim.x  # pocet vlakien v bloku
+    pos = tx + ty * bw
+
+    if pos < io_array.size:
+        io_array[pos] *= 2
+
+
+def main():
+    data = numpy.ones(256)
+    threads_per_block = 256
+    blocks_per_grid = math.ceil(data.shape[0] / threads_per_block)
+    my_kernel[blocks_per_grid, threads_per_block](data)
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
